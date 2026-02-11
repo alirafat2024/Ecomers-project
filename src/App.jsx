7ece57aa31996b8b908c02c0b371392be3838895
alirafat2024 <mohammadalirafat102@gmail.com>
2026-02-11 10:04:27 -0800

    add product on the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes,Route } from 'react-router'
 import { HomePage } from './pages/homePage'
-import { Checkout } from './pages/CheckoutPage'
+import { Checkout } from './pages/checkout/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
 import { Tracking } from './pages/TrackingPage'
 import './App.css'
