3c3b782fc37535d8904aacfe257a33750673f694
alirafat2024 <mohammadalirafat102@gmail.com>
2026-07-08 19:25:16 +0430

    Generated HTM for the orders page

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <Route path="/" element={<HomePage  cart={cart}/>} />
 
       <Route path="checkout" element={<Checkout  cart={cart}/>} />
-      <Route path="orders" element={<OrdersPage />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
       <Route path="tracking" element={<Tracking />} />
     </Routes>
   );
